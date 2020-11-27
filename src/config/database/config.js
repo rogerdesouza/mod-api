40c5b5acae20e5a6e0f010efc5ed0a9f1652a762
roger <roger@policiacivil.pe.gov.br>
2020-11-27 14:08:14 -0300

    ajustes de banco de dados

---
+++ src/config/database/config.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+module.exports = {
+    development: {
+        database: process.env.DB_NAME || 'mod',
+        username: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASS || '123',
+        host: process.env.DB_HOST || '0.0.0.0',
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || 'postgres',
+        logging: true,
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
+        }
+    },
+    test: {
+        database: process.env.DB_NAME || 'mod_test',
+        username: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASS || '123',
+        host: process.env.DB_HOST || '0.0.0.0',
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || 'postgres',
+        logging: true,
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
+        }
+    },
+    production: {
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true
+        }
+    }
+}
